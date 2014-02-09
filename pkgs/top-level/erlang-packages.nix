0168ebec2ee9dad146e74be6e67e1d8dcb134771
Tristan Sloughter <t@crashfast.com>
2014-02-09 14:08:31 -0600

    add goldrush package

--- pkgs/top-level/erlang-packages.nix
+++ pkgs/top-level/erlang-packages.nix
@@ -1,7 +1,6 @@
 { pkgs, erlang }:
 
 rec {
-
   inherit erlang;
   inherit (pkgs) fetchurl fetchsvn fetchgit stdenv;
 
@@ -12,6 +11,19 @@ rec {
   buildErlangPackage = callPackage ../development/erlang-modules/generic { };
 
   # packages defined here
+
+  goldrush = buildErlangPackage {
+    name = "goldrush-0.1.6";
+    src = fetchurl {
+      url = https://github.com/DeadZen/goldrush/archive/0.1.6.tar.gz;
+      sha256 = "554f36983e19fcf2ebee18b5c0ff502af4fc1b853b49041c7a9821d7043e349f";
+    };
+
+    meta = {
+      description = "Small, Fast event processing and monitoring for Erlang/OTP applications.";
+      license = "";
+    };
+  };
 
   heroku_crashdumps = buildErlangPackage {
     name = "heroku_crashdumps-0.1.0";
